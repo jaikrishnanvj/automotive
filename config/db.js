a6f78c87a8a417ed17ba60c4fa111857628134d9
jaikrishnan <vjjaikrishnan7890@gmail.com>
2024-04-15 11:45:21 +0530

    file pushed

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 const connectdb= ()=> {
     
-    mongoose.connect("mongodb://127.0.0.1:27017/projects");
+    mongoose.connect(process.env.MONGO_URI)
 }
 module.exports=connectdb
 
